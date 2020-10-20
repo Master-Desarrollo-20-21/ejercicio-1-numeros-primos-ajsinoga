91d0495ce93d5ab0989dcab70c80969e60a4f483
ajsinoga <ajsinoga@gmail.com>
2020-10-20 19:54:46 +0200

    Ejercicio números primos en C#

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace ejercicio_1_numeros_primos_ajsinoga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcion = usage();
+
+            switch(opcion)
+            {
+                case "1":
+                    Console.WriteLine("Suma 50 primeros números primos = " + SumOf50FirstPrimeNumbers().ToString());
+                    break;
+                case "2":
+                    Console.WriteLine("Suma de los primos que hay en los 50 primeros números = " + SumOfPrimeNumbersIn50FirstNumbers().ToString());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static string usage()
+        {
+            string opcion = "";
+
+            Console.WriteLine("\nElige 1 para la suma de los 50 primeros números primos.\nElige 2 para la suma de los primos que hay en los 50 primeros números...");
+            opcion = Console.ReadLine();
+            while (!opcion.Equals("1") && !opcion.Equals("2"))
+            {
+                opcion = Console.ReadLine();
+            }
+            
+            return opcion;            
+        }
+
+        static int SumOf50FirstPrimeNumbers()
+        {
+            int number = 1;
+            int totalPrimeNumbers = 0;
+            int sumTotal = 0;
+            
+            while (totalPrimeNumbers <= 50)
+            {
+                if (isPrime(number))
+                {
+                    sumTotal = sumTotal + number;
+                    totalPrimeNumbers ++;
+                }
+                number ++;
+            }
+
+            return sumTotal;
+        }   /* resultado 5350 */
+
+        static int SumOfPrimeNumbersIn50FirstNumbers()
+        {    
+            int sumTotal = 0;
+            
+            for (int number = 1; number <= 50; number++)
+            {
+                if (isPrime(number))
+                {
+                    sumTotal = sumTotal + number;
+                }
+            }
+
+            return sumTotal;    
+        }  /* resultado 328 */
+
+        static bool isPrime(int number)
+        {   
+            if (number < 2)
+            {
+                return false;
+            }
+            /* Un número es primo si es divisible únicamente por 1 y por si mismo */
+            /* pero cuando superamos la mitad ya no tiene divisores de resto 0 */
+            for (int divisor = 2; divisor <= number / 2; divisor++)
+            { 
+                if (number % divisor == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
